f8ef47757fea343c8a77bf2b4739c4b64c01a656
Kristjan Toots <kristjan.toots@gmail.com>
2017-10-25 19:29:51 +0300

    #1 Create new solution & project. Print out text to illustrate program execution

---
+++ src/03-dice-roller/DiceRoller/DiceRoller/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace DiceRoller
+{
+    using System;
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("> /roll 3d6 2d8");
+            Console.WriteLine();
+
+            Console.WriteLine("1d6: 5");
+            Console.WriteLine("1d6: 3");
+            Console.WriteLine("1d6: 2");
+            Console.WriteLine("1d8: 6");
+            Console.WriteLine("1d8: 4");
+
+            Console.WriteLine();
+            Console.WriteLine($"Roll total: 20");
+            Console.WriteLine();
+            Console.Write("> ");
+
+            Console.ReadLine();
+        }
+    }
+}
