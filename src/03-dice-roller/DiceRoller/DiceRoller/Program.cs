663c512933502c1e97013872ee45f75554aa6aa9
Kristjan Toots <kristjan.toots@gmail.com>
2017-10-25 20:28:34 +0300

    #7 Create Dice object. Refactor 'Program.cs' so that there is no logic for generating random numbers

--- src/03-dice-roller/DiceRoller/DiceRoller/Program.cs
+++ src/03-dice-roller/DiceRoller/DiceRoller/Program.cs
@@ -9,12 +9,13 @@
             Console.WriteLine("> /roll 3d6 2d8");
             Console.WriteLine();
 
-            var random = new Random();
+            Dice dice = new Dice();
+
             var total = 0;
 
             for (var i = 0; i < 3; i++)
             {
-                var roll = random.Next(1, 7);
+                var roll = dice.Roll(6);
                 total += roll;
 
                 Console.WriteLine($"1d6: { roll }");
@@ -22,7 +23,7 @@
 
             for (var i = 0; i < 2; i++)
             {
-                var roll = random.Next(1, 9);
+                var roll = dice.Roll(8);
                 total += roll;
 
                 Console.WriteLine($"1d8: { roll }");
