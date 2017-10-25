1c9d0e38ec8504e028602e0cf389d231fffeb484
Kristjan Toots <kristjan.toots@gmail.com>
2017-10-25 19:18:38 +0300

    Add folder structure & placeholder for graded assignment

---
+++ src/02-graded-assignment/GradedAssignment/GradedAssignment/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace GradedAssignment
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //Program prints out information that it is a discount calculator and prompts user to provide input for a sum
+            
+            //Rules: 
+            //+50$ the discount is 5 %
+            //+250$ the discount is 10 %
+            //+500$ the discount is 20 %
+            
+            //Program prints out entered sum, discount percent and sum which would have to be paid after the discount is reduced
+        }
+    }
+}
