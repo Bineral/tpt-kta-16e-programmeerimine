6a73f52b7145206b60f93052cf9591f829bbc910
Kristjan Toots <kristjan.toots@gmail.com>
2017-10-25 23:04:30 +0300

    #16 Create DiceRoll constructor and implement DiceRoller.Roll method

--- src/03-dice-roller/DiceRoller/DiceRoller/DiceRoll.cs
+++ src/03-dice-roller/DiceRoller/DiceRoller/DiceRoll.cs
@@ -5,5 +5,11 @@
         public string Dice { get; }
 
         public int Value { get; }
+
+        public DiceRoll(string dice, int value)
+        {
+            this.Dice = dice;
+            this.Value = value;
+        }
     }
 }
