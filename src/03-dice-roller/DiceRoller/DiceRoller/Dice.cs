7e51beeb29cc2fee4ea134d0db6bd1f0b12ff05d
Kristjan Toots <kristjan.toots@gmail.com>
2017-10-25 20:19:34 +0300

    #6 Add Roll() method to the Dice which accepts number of sides and returns corresponding random number

--- src/03-dice-roller/DiceRoller/DiceRoller/Dice.cs
+++ src/03-dice-roller/DiceRoller/DiceRoller/Dice.cs
@@ -1,7 +1,14 @@
 ﻿namespace DiceRoller
 {
+    using System;
+
     public class Dice
     {
-        
+        public int Roll(int sides)
+        {
+            var random = new Random();
+
+            return random.Next(1, sides + 1); // max value is exclusive, so we have to add +1
+        }
     }
 }
