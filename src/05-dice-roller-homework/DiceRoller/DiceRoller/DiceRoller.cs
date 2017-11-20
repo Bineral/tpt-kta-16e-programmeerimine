0c59f406543e71f21777e6720f4922191d4e5615
Kristjan Toots <kristjan.toots@gmail.com>
2017-11-20 14:26:54 +0200

    Original solution

---
+++ src/05-dice-roller-homework/DiceRoller/DiceRoller/DiceRoller.cs
@@ -0,0 +1,19 @@
+﻿namespace DiceRoller
+{
+    using System.Collections.Generic;
+
+    public class DiceRoller
+    {
+        public List<DiceRoll> Roll(List<Dice> dices)
+        {
+            var result = new List<DiceRoll>();
+
+            foreach (var dice in dices)
+            {
+                result.Add(new DiceRoll(dice.Description, dice.Roll()));
+            }
+
+            return result;
+        }
+    }
+}
