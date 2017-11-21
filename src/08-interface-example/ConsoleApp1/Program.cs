ae08dee308b5be60c422742eb26ba93bc2879b25
Kristjan Toots <kristjan.toots@gmail.com>
2017-11-21 09:49:25 +0200

    Interface example

---
+++ src/08-interface-example/ConsoleApp1/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace ConsoleApp1
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var list = new List<Animal> { new Dog(), new Cat(), new Duck() };
+
+            foreach (var animal in list)
+            {
+                Console.WriteLine(animal.MakeSound());
+            }
+
+            Console.ReadKey();
+        }
+    }
+
+    public class Duck : Animal
+    {
+        public string MakeSound()
+        {
+            return "Quack!";
+        }
+    }
+
+    public class Cat : Animal
+    {
+        public string MakeSound()
+        {
+            return "Mjaow!";
+        }
+    }
+
+    public class Dog : Animal
+    {
+        public string MakeSound()
+        {
+            return "Wooof!";
+        }
+    }
+
+    public interface Animal
+    {
+        string MakeSound();
+    }
+
+}
+
+
