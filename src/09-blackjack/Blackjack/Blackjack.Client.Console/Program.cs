e91dcf9c636c873851a8bed0d7f8caf29a6d9471
Kristjan Toots <kristjan.toots@gmail.com>
2017-11-21 13:03:45 +0200

    Introduce Enum Suite instead of relying on a single char

--- src/09-blackjack/Blackjack/Blackjack.Client.Console/Program.cs
+++ src/09-blackjack/Blackjack/Blackjack.Client.Console/Program.cs
@@ -14,16 +14,16 @@
             Console.WriteLine("Welcome to the game of Blackjack!");
             Console.WriteLine();
 
-            var c4 = new Card("4", 'C', 4);
+            var c4 = new Card("4", Suite.Club, 4);
             c4.Hidden = false;
 
-            var h6 = new Card("6", 'H', 6);
+            var h6 = new Card("6", Suite.Heart, 6);
             h6.Hidden = false;
 
-            var s8 = new Card("8", 'S', 8);
+            var s8 = new Card("8", Suite.Spade, 8);
             s8.Hidden = false;
 
-            var dJ = new Card("J", 'D', 10);
+            var dJ = new Card("J", Suite.Diamond, 10);
 
             Console.WriteLine($"You have been dealt: {GetCardDescription(c4)}, {GetCardDescription(h6)}");
             Console.WriteLine($"House has been dealt: {GetCardDescription(s8)}, { GetCardDescription(dJ)}");
@@ -36,10 +36,10 @@
             Console.WriteLine("I choose: 1");
             Console.WriteLine();
 
-            var s4 = new Card("4", 'S', 4);
+            var s4 = new Card("4", Suite.Spade, 4);
             s4.Hidden = false;
 
-            var hT = new Card("T", 'H', 10);
+            var hT = new Card("T", Suite.Heart, 10);
 
             Console.WriteLine($"You have been dealt: {GetCardDescription(s4)}");
             Console.WriteLine($"House has been dealt: {GetCardDescription(hT)}");
